a0bebc50673bc723b2a23e2e8d48023cb37a5d8f
Vincent M <vincentmartins52@yahoo.fr>
2022-03-22 10:54:23 +0100

    Intégration env

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,4 +1,38 @@
-const sequelize = new Sequelize("database", "username", "password", {
-  host: "localhost",
-  dialect: "mysql",
-});
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
+
+/**
+ * users :
+ *  - id
+ *  - email
+ *  - password
+ *  - picture
+ *  - biography
+ *  -role
+ *
+ * posts :
+ *  - id
+ *  - user_id
+ *  - content
+ *  - picture (url)
+ *  - created_at
+ *
+ * comments :
+ *  - id
+ *  - post_id
+ *  - user_id
+ *  - content
+ *  - picture ?? à vérifier
+ *
+ *
+ *
+ */
