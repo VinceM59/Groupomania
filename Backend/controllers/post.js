ac712156c048a62e8f0f2809e5dfe39b4c006f87
Vincent M <vincentmartins52@yahoo.fr>
2022-03-26 12:47:46 +0100

    Ajout controllers et routes pour post

--- Backend/controllers/post.js
+++ Backend/controllers/post.js
@@ -1,4 +1,5 @@
 const models = require("../models");
+const like = require("../models/like");
 const Post = models.posts;
 const Comment = models.comments;
 const Like = models.likes;
@@ -14,4 +15,73 @@ exports.createPost = (req, res, next) => {
     .save()
     .then(() => res.status(201).json({ message: "Post publié !" }))
     .catch((error) => res.status(400).json({ error }));
+};
+
+exports.findOnePost = (req, res, next) => {
+  Post.findOne({ where: { id: req.params.id } })
+    .then((post) => {
+      console.log(post);
+      res.status(200).json(post);
+    })
+    .catch((error) => res.status(404).json({ error }));
+};
+
+exports.modifyPost = (req, res, next) => {
+  const postObject = req.file
+    ? {
+        ...req.body.post,
+        image: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
+  postUpdateOne(
+    { ...postObject, id: req.params.id },
+    { where: { id: req.params.id } }
+  )
+    .then(() => res.status(200).json({ message: "Post modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.deletePost = (req, res, next) => {
+  Comment.destroy({ where: { postId: req.params.id } })
+    .then(() =>
+      like
+        .destroy({ where: { postId: req.params.id } })
+        .then(() =>
+          Post.destroy({ where: { id: req.params.id } }).then(() =>
+            res.status(200).json({ message: "Post supprimé !" })
+          )
+        )
+    )
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.findAllPosts = (req, res, next) => {
+  Post.findAll({
+    order: [["id", "DESC"]],
+  })
+    .then((posts) => {
+      res.status(200).json({ posts });
+    })
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.findAllPostsUser = (req, res, next) => {
+  Post.findAll({
+    where: { userId: req.params.id },
+    order: [["id", "DESC"]],
+  })
+    .then((post) => {
+      res.status(200).json({ posts });
+    })
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.findOne = (req, res, next) => {
+  Post.findOne({ where: { id: req.params.id } })
+    .then((post) => {
+      res.status(200).json(post);
+    })
+    .catch((error) => res.status(404).json({ error }));
 };
--- Backend/routes/post.js
+++ Backend/routes/post.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const postCtrl = require("../controllers/post");
+
+router.get("/", postCtrl.findAllPosts);
+router.post("/", postCtrl.createPost);
+router.get("/:id", postCtrl.findOne);
+router.put("/:id", postCtrl.modifyPost);
+router.delete("/:id", postCtrl.deletePost);
 
 module.exports = router;
