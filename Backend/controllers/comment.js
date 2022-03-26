53418d98489833087a96f9e7ffae2cba2feec9ed
Vincent M <vincentmartins52@yahoo.fr>
2022-03-26 17:22:00 +0100

    Ajout controllets et routes comment

---
+++ Backend/controllers/comment.js
@@ -0,0 +1,49 @@
+const models = require("../models");
+const Comment = models.comments;
+
+exports.createComment = (req, res, next) => {
+  const comment = new Comment({
+    content: req.body.content,
+    userId: req.body.userId,
+    postId: req.body.userId,
+  });
+  comment
+    .save()
+    .then(() => res.status(201).json({ message: "Commentaire ajouté !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.findOneComment = (req, res, next) => {
+  Comment.findOne({ where: { id: req.params.id } })
+    .then((comment) => {
+      console.log(comment);
+      res.status(200).json(comment);
+    })
+    .catch((error) => res.status(404).json({ error }));
+};
+
+//modify ?????
+
+exports.deleteComment = (req, res, next) => {
+  Comment.destroy({ where: { id: req.params.id } })
+    .then(() => res.status(200).json({ message: "Commentaire supprimé !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.findAllComment = (req, res, next) => {
+  Post.findAll({
+    where: { postId: req.params.id },
+  })
+    .then((comments) => {
+      res.status(200).json({ comments });
+    })
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.findOneComment = (req, res, next) => {
+  Post.findOneComment({ where: { id: req.params.id } })
+    .then((comment) => {
+      res.status(200).json(comment);
+    })
+    .catch((error) => res.status(404).json({ error }));
+};
--- Backend/routes/comment.js
+++ Backend/routes/comment.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const commentCtrl = require("../controllers/comment");
 
+router.get("/", commentCtrl.findAllComments);
+router.post("/", commentCtrl.createComment);
+router.get("/:id", commentCtrl.findOneComment);
+router.delete("/:id", commentCtrl.deleteComment);
 module.exports = router;
