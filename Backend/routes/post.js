707ec58161073f0acb747d181d0482912fd8419d
Vincent M <vincentmartins52@yahoo.fr>
2022-03-27 14:35:28 +0200

    Ajout de Multer

--- Backend/routes/post.js
+++ Backend/routes/post.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const postCtrl = require("../controllers/post");
 
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 router.get("/", auth, postCtrl.findAllPosts);
-router.post("/", auth, postCtrl.createPost);
+router.post("/", auth, multer, postCtrl.createPost);
 router.get("/:id", auth, postCtrl.findOne);
-router.put("/:id", auth, postCtrl.modifyPost);
+router.put("/:id", auth, multer, postCtrl.modifyPost);
 router.delete("/:id", auth, postCtrl.deletePost);
 
 module.exports = router;
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const userCtrl = require("../controllers/user");
 
 const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 router.get("/", auth, userCtrl.findAllUsers);
 router.get("/:id", auth, userCtrl.findOneUser);
 router.get("/:id", auth, userCtrl.findAllUserName);
-router.put("/;id", auth, userCtrl.modifyUser);
+router.put("/;id", auth, multer, userCtrl.modifyUser);
 
 module.exports = router;
