33d339ce874e78bc1adb317beb3773bf93011e29
Vincent M <vincentmartins52@yahoo.fr>
2022-04-01 17:26:52 +0200

    Début Signup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import Signup from "../views/Signup-form.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/signup-form",
+    name: "Signup-form",
+    component: Signup,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ frontend/src/views/Signup-form.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+      <div class = "register">
+          <p class ="register_text">salut !</p>
+      </div> 
+      <div class="main-w3layouts wrapper">
+		<h1>Inscription</h1>
+		<div class="main-agileinfo">
+			<div class="agileits-top">
+				<form action="#" method="post">
+					<input class="text" type="text" name="Lastname" placeholder="Lastname" required="">
+					<input class="text" type="text" name="Firstname" placeholder="Firstname" required="">
+					<input class="text email" type="email" name="email" placeholder="Email" required="">
+					<input class="text" type="password" name="password" placeholder="Password" required="">
+					<input class="text w3lpass" type="password" name="password" placeholder="Confirm Password" required="">
+					<div class="wthree-text">
+						<label class="anim">
+							<input type="checkbox" class="checkbox" required="">
+							<span>I Agree To The Terms & Conditions</span>
+						</label>
+						<div class="clear"> </div>
+					</div>
+					<input type="submit" value="SIGNUP">
+				</form>
+				<p>Don't have an Account? <a href="#"> Login Now!</a></p>
+			</div>
+		</div> 
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Signup-form'
+
+}
+</script>
+
+<style scoped>
+
+</style>
